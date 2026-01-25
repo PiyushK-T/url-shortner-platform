fb79c4b78f545d146e33ebd3ecf244f024139e06
Piyush Kataktalware <69578342+PiyushK-T@users.noreply.github.com>
2026-01-24 19:09:12 -0500

    fix: load test env vars for Jest

--- services/analytics-service/jest.setup.ts
+++ services/analytics-service/jest.setup.ts
@@ -3,8 +3,3 @@ import dotenv from "dotenv";
 dotenv.config({
   path: ".env.test"
 });
-
-process.env.PORT = "3000";
-process.env.AUTH_SERVICE_URL = "http://localhost:4001";
-process.env.URL_SERVICE_URL = "http://localhost:4002";
-process.env.ANALYTICS_SERVICE_URL = "http://localhost:4003";
--- services/auth-service/jest.setup.ts
+++ services/auth-service/jest.setup.ts
@@ -1,6 +1,2 @@
 import dotenv from "dotenv";
 dotenv.config({ path: '.env.test' });
-process.env.PORT = "3000";
-process.env.AUTH_SERVICE_URL = "http://localhost:4001";
-process.env.URL_SERVICE_URL = "http://localhost:4002";
-process.env.ANALYTICS_SERVICE_URL = "http://localhost:4003";
